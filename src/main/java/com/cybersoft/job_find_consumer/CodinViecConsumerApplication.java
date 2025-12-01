ffa841be6d1a9bbd1c11240946add01825e27b6a
Phu Nguyen <trungphuntp@gmail.com>
2025-12-01 15:48:00 +0700

    fix name project

--- src/main/java/com/cybersoft/job_find_consumer/CodinViecConsumerApplication.java
+++ src/main/java/com/cybersoft/job_find_consumer/CodinViecConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobFindConsumerApplication {
+public class CodinViecConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobFindConsumerApplication.class, args);
+		SpringApplication.run(CodinViecConsumerApplication.class, args);
 	}
 
 }
